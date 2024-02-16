88cd63c26b19583ebaf1214e38b1d459f6ca5451
Danilo Glushko <39314465+BigBlueDraco@users.noreply.github.com>
2024-02-16 02:06:23 +0200

    add: env and readme

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { RoleGuard } from './auth/guards/role.guard';
+import 'dotenv/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,7 +16,12 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('api', app, document);
+  app.enableCors({
+    origin: [...process.env.CORS.split(' ')],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
